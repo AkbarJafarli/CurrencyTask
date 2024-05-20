ce4c0c31f7decb0bf1a8f028c28de3ef7fc88d5f
Akbar Jafarli <akbaraj@code.edu.az>
2024-05-20 13:29:48 +0400

    Currency rate based calculation was completed

--- Jafarlee/Program.cs
+++ Jafarlee/Program.cs
@@ -706,35 +706,36 @@ namespace Jafarlee
                 Console.WriteLine("This exchange rate was not found in the system");
             }
             Console.WriteLine("=====================");
-            //Console.WriteLine("3)Enter the amount");
-            //decimal amount;
-            //amount = decimal.Parse(Console.ReadLine());
-            //Console.WriteLine("Enter the exchange rate code");
-            //string excCode;
-            //excCode = Console.ReadLine();
-            //string[] lastCodes = { "USD", "EUR", "AED", "CZK", "CNY", "CAD", "MXN", "RUB", "TRY", "UAH" };
-            //int lastCodesIdx = 0;
-            //decimal[] lastRates = { 1.7000M, 1.8455M, 0.4628M, 0.0748M, 0.2352M, 1.2477M, 0.1017M, 0.0187M, 0.0527M, 0.0432M };
-            //int lastRatesIdx = 0;
-            //decimal sumAmount;
-            //while (lastCodesIdx < lastCodes.Length)
-            //{
-            //    if (excCode == lastCodes[lastCodesIdx])
-            //    {
-            //        sumAmount = amount * lastRates[lastCodesIdx];
-            //        Console.WriteLine(sumAmount);
-            //        break;
-            //    }
-            //    else
-            //    {
-            //        lastCodesIdx++;
-            //        lastRatesIdx++;
-            //    }
-            //}
-            //if (lastRatesIdx == lastCodes.Length)
-            //{
-            //    Console.WriteLine("This exchange rate was not found in the system");
-            //}
+            Console.WriteLine("3)Enter the amount");
+            decimal amount;
+            amount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the exchange rate code");
+            string excCode;
+            excCode = Console.ReadLine();
+            string[] lastCodes = { "USD", "EUR", "AED", "CZK", "CNY", "CAD", "MXN", "RUB", "TRY", "UAH" };
+            int lastCodesIdx = 0;
+            decimal[] lastRates = { 1.7000M, 1.8455M, 0.4628M, 0.0748M, 0.2352M, 1.2477M, 0.1017M, 0.0187M, 0.0527M, 0.0432M };
+            int lastRatesIdx = 0;
+            decimal sumAmount;
+            while (lastCodesIdx < lastCodes.Length)
+            {
+                if (excCode == lastCodes[lastCodesIdx])
+                {
+                    sumAmount = amount * lastRates[lastCodesIdx];
+                    Console.WriteLine(sumAmount);
+                    break;
+                }
+                else
+                {
+                    lastCodesIdx++;
+                    lastRatesIdx++;
+                }
+            }
+            if (lastRatesIdx == lastCodes.Length)
+            {
+                Console.WriteLine("This exchange rate was not found in the system");
+            }
+
         }
     }
 }
