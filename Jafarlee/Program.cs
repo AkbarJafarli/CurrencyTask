901e36cef60fc025c414eb3131c41653774aeef6
Akbar Jafarli <akbaraj@code.edu.az>
2024-05-20 13:24:01 +0400

    Showing specific currency related info was completed

--- Jafarlee/Program.cs
+++ Jafarlee/Program.cs
@@ -680,33 +680,32 @@ namespace Jafarlee
                     break;
                 }
             }
-            //Console.WriteLine("=====================");
-            //Console.WriteLine("2)Find currency rate by code");
-            //string codes = Console.ReadLine();
-            //string[] currencyCodes2 = { "USD", "EUR", "AED", "CZK", "CNY", "CAD", "MXN", "RUB", "TRY", "UAH" };
-            //int currencyCodesIdx2 = 0;
-            //decimal[] currencyRates2 = { 1.7000M, 1.8455M, 0.4628M, 0.0748M, 0.2352M, 1.2477M, 0.1017M, 0.0187M, 0.0527M, 0.0432M };
-            //int currencyRatesIdx2 = 0;
-            //bool isExist = false;
-            //while (currencyCodesIdx2 < currencyCodes2.Length)
-            //{
-            //    if (codes == currencyCodes2[currencyCodesIdx2])
-            //    {
-            //        Console.WriteLine(currencyCodes2[currencyCodesIdx2] + "= " + currencyRates2[currencyRatesIdx2]);
-            //        break;
-            //    }
-            //    else
-            //    {
-            //        currencyCodesIdx2++;
-            //        currencyRatesIdx2++;
-            //    }
-            //}
-            //if (currencyCodesIdx2 == currencyCodes2.Length)
-            //{
-            //    Console.WriteLine("This exchange rate was not found in the system");
-            //}
-            //Console.WriteLine("=====================");
-
+            Console.WriteLine("=====================");
+            Console.WriteLine("2)Find currency rate by code");
+            string codes = Console.ReadLine();
+            string[] currencyCodes2 = { "USD", "EUR", "AED", "CZK", "CNY", "CAD", "MXN", "RUB", "TRY", "UAH" };
+            int currencyCodesIdx2 = 0;
+            decimal[] currencyRates2 = { 1.7000M, 1.8455M, 0.4628M, 0.0748M, 0.2352M, 1.2477M, 0.1017M, 0.0187M, 0.0527M, 0.0432M };
+            int currencyRatesIdx2 = 0;
+            bool isExist = false;
+            while (currencyCodesIdx2 < currencyCodes2.Length)
+            {
+                if (codes == currencyCodes2[currencyCodesIdx2])
+                {
+                    Console.WriteLine(currencyCodes2[currencyCodesIdx2] + "= " + currencyRates2[currencyRatesIdx2]);
+                    break;
+                }
+                else
+                {
+                    currencyCodesIdx2++;
+                    currencyRatesIdx2++;
+                }
+            }
+            if (currencyCodesIdx2 == currencyCodes2.Length)
+            {
+                Console.WriteLine("This exchange rate was not found in the system");
+            }
+            Console.WriteLine("=====================");
             //Console.WriteLine("3)Enter the amount");
             //decimal amount;
             //amount = decimal.Parse(Console.ReadLine());
